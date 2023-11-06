d316a8e7de7c2c6ed3e9e72fa16ab7b0717bb579
pikari1212 <r202301955jd@jindai.jp>
2023-11-06 16:59:53 +0900

    20231106

--- de12/python/main copy 2.py
+++ de12/python/main copy 2.py
@@ -1,15 +1,36 @@
-import wikipedia
+import japanize_kivy
+import urllib.parse
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+import webbrowser
+kivy.require('1.11.1')
 
-# キーワードを設定
-keyword = "犬"
-# キーワードで検索
-wikipedia.set_lang("ja")
-result = wikipedia.search(keyword)
-print("検索結果",result)
+class HashTagSearchApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.links = []
 
-print("最初の検索結果を表示")
-page_data = wikipedia.page(result[0])
-print(page_data.content)
+    def build(self):
+        self.layout = BoxLayout(orientation='vertical')
+        self.input_text = TextInput(hint_text='enter a hashtag without "#"')
+        self.add_button = Button(text='search')
+        self.add_button.bind(on_press=self.search_hashtag)
+        self.layout.add_widget(self.input_text)
+        self.layout.add_widget(self.add_button)
+        return self.layout
+
+    def search_hashtag(self, instance):
+        hashtag = self.input_text.text.strip()
+        if hashtag:
+            hashtag = urllib.parse.quote(hashtag)  # キーワードをエンコード
+            search_url = f'https://bizwd.net/'
+            webbrowser.open(search_url)
+
+if __name__ == '__main__':
+    HashTagSearchApp().run()
 
 
 
